bd5d2556bcac4ae69dec9c3bea9eb9a7789238d0
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-24 09:22:46 -0400

    added constructor commit.

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class Product {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String name, String id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+
+
 }
